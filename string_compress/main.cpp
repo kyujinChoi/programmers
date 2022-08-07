8552b3ed6ef8cf40e9dd57d9a1ce887d87a31834
kyujinChoi <qjinchoi@gmail.com>
2022-08-07 16:04:46 +0900

    commit

--- string_compress/main.cpp
+++ string_compress/main.cpp
@@ -3,87 +3,48 @@
 #include <iostream>
 
 
-int solution(std::string s) 
+std::string solution(std::string s) 
 {
     int answer = s.length();
     int min = s.length();
-
-    for (int str_size = 1; str_size <= s.length(); i++)
+    std::string ret_str;
+    for (int str_size = 1; str_size <= s.length(); str_size++)
     {
-        for(int pos = 0; pos < s.length(); pos++)
+        std::string pivot_str = s.substr(0, str_size);
+        int find_idx = str_size;
+        int same_cnt = 1;
+        while(1)
         {
-            std::string pivot_str;
+            if(find_idx > s.length() / 2)
+                return ret_str;
+
+            std::string find_str = s.substr(find_idx, str_size);
+
+            if(pivot_str == find_str)
+            {
+                same_cnt++;
+                pivot_str = s.substr(find_idx + str_size, str_size);
+
+            }
+            else
+            {
+
+            }
+            find_idx += str_size;
         }
+        std::cout << "ret_str : " << ret_str << std::endl;
     }
 
-    return answer;
+    return ret_str;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 int main()
 {
     std::string input1 = "aabbaccc";
-    int result;
+    std::string result;
 
-
+    std::cout << "input : " << input1 << std::endl;
     result = solution(input1);
     std::cout << result << "\n";
     // 7
